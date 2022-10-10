68140e2fe681f79c2d37d0a78bdd1dcc2ab1849f
rayhonaistamova <istamovarayhona644@gmail.com>
2022-10-10 14:47:58 +0500

    homework have done

---
+++ main.js
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+// Algorithm 1
+
+// let ombor = {
+//     moy: ['pista', 'paxta', 'semechka'],
+//     gosht: ['mol', 'qoy', 'echki', 'tovuq'],
+//     masalliq: ['kartoshka', 'bodiring', 'piyoz', 'sabzi'],
+//     ziravor: ['tuz', 'uksus', 'zira', 'kunjut', 'kashnich']
+// }
+// function ovqatTayyorla(index, call_production) {
+//     setTimeout(() => {
+//         console.log(`birinchi ${ombor.moy[index]} solinadi`);
+//     }, 1000)
+//     call_production()
+// }
+
+// let tayyorla = () => {
+//     setTimeout(() => {
+//         console.log("Yog qizishini kutamiz");
+//         setTimeout(() => {
+//             console.log(`keyin ${ombor.gosht[1]} goshti solamiz`);
+//             setTimeout(() => {
+//                 console.log("gosht qizarib qovuriladi");
+//                 setTimeout(() => {
+//                     console.log(`ustidan ${ombor.masalliq[2]} solinadi`);
+//                     setTimeout(() => {
+//                         console.log(`piyoz qovurilgandan song ${ombor.masalliq[0]} solinadi`);
+//                         setTimeout(() => {
+//                             console.log(`ustidan ${ombor.ziravor[0]} solib dimlab qoyamiz`);
+//                         }, 0000)
+//                     }, 2000)
+//                 }, 1000)
+//             }, 3000)
+//         }, 1000)
+//     }, 2000)
+// }
+
+// ovqatTayyorla(2, tayyorla)
+
+/////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
